5b2a665ce0d04dabac13aecbe2820ebbcc15d7aa
Héverton Oliveira <heverton.ogs@gmail.com>
2021-10-04 18:29:53 -0400

    remove: entity UserToken not used by the project

--- src/shared/container/index.ts
+++ src/shared/container/index.ts
@@ -5,20 +5,12 @@ import '@modules/users/providers';
 import IUsersRepository from '@modules/users/repositories/IUsersRepository';
 import UsersRepository from '@modules/users/infra/typeorm/repositories/UsersRepository';
 
-import IUserTokensRepository from '@modules/users/repositories/IUserTokensRepository';
-import UserTokensRepository from '@modules/users/infra/typeorm/repositories/UserTokensRepository';
-
 import IAddressRepository from '@modules/address/repositories/IAddressRepository';
 import AddressRepository from '@modules/address/infra/typeorm/repositories/AddressRepository';
 
 container.registerSingleton<IUsersRepository>(
   'UsersRepository',
   UsersRepository,
-);
-
-container.registerSingleton<IUserTokensRepository>(
-  'UserTokensRepository',
-  UserTokensRepository,
 );
 
 container.registerSingleton<IAddressRepository>(
